40be5de5df5895acdb45088aa55be824be6a94ca
Duane Felton <dfelton2010@gmail.com>
2018-05-17 14:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // write your code below!
 
 var name = "Duane"
+<<<<<<< HEAD
 var height = 71
+=======
+var height = "71"
+>>>>>>> 7a5ad857e98731818c02af8ce98ca389887a6929
 
 console.log("Name:")
 console.log(name)
